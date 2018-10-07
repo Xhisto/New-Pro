b3f570fb467f90401c6415737c172e00fbd147b8
CripC <Alrik-Mark.Brillantes@elev.ga.lbs.se>
2018-10-07 15:30:03 +0200

    Jag är klar
    Har gjort allting, jag är ett A barn.

--- Assets/Easy.cs
+++ Assets/Easy.cs
@@ -12,15 +12,25 @@ public class Easy : MonoBehaviour
     public Transform other;
     public float Timer;
     public float leftwarp = -7.5f;
+    public float upwarp = 5.5f;
+    public float downwarp = -5.5f;
     public float rigthwarp = 7.5f;
-    public float disapear = 1f;
-    readonly object AtomBomb;
+    public float ColorR;
+    public float ColorG;
+    public float ColorB;
+    public float PositionX;
+    public float PositionY;
 
     // Use this for initialization
     void Start()
     {
-        other.position = new Vector3(0, 0, 0);
-
+        PositionX = Random.Range(-6f, 6f);
+        PositionY = Random.Range(-4.5f, 4.5f);
+        // ger mina random values till postioneerna X och Y, men Z måste alltid vara över Z annars overlappar min camera med mitt skepp.
+        other.position = new Vector3(PositionX, PositionY, 0f);
+        //vart skeppet kommer starta i början av spelet.
+        Speed = Random.Range(10, 15);
+        // min skepps hastighets random generator.
     }
 
     // Update is called once per frame
@@ -28,11 +38,10 @@ public class Easy : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0f, 0f, RotationSpeed * Time.deltaTime);
+            transform.Rotate(0f, 0f, (RotationSpeed / 2) * Time.deltaTime);
             Rend.color = new Color(0f, 0f, 200f);
         }
-
-
+        
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0f, 0f, -RotationSpeed * Time.deltaTime);
@@ -45,13 +54,27 @@ public class Easy : MonoBehaviour
             transform.Translate(-Speed / 2 * Time.deltaTime, 0, 0, Space.Self);
         }
 
-        transform.Translate(Speed * Time.deltaTime, 0, 0, Space.Self);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ColorR = Random.Range(0f, 1f);
+            ColorG = Random.Range(0f, 1f);
+            ColorB = Random.Range(0f, 1f);
+            // ger röd, grön och blå random values för att randomize:a det resulterade färgen. 
+            Rend.color = new Color(ColorR, ColorG, ColorB, 1f);
+            // lägger till det resulterade färgen
+        }
 
+        transform.Translate(Speed * Time.deltaTime, 0, 0, Space.Self);
+        // Transform.rotate är det som gör så att skeppet svänger vänster och höger. 
+        // Och Transform.Translate är det som bestämmer hur snabbt eller långsam det är.
+        // Rend.color visar vilken färg det byts till.
+        // if koderna visar bara vad som händer om jag trycker på det valda keyboards knappen.
+        // get key checkar förendringar på när knappen trycks ner och släpps up, medan getkeydown och getkeydown checkar bara de.
 
         Timer += Time.deltaTime;
-
-        Debug.Log(string.Format("Time (i sekunder och decimaler.... FOR NOW ):{0}", +Timer));
-
+        // koden för timer i sekunder istället per frame
+        print((int)Timer);
+        // printar timer i heltal
 
         if (other.position.x < -7.5f)
         {
@@ -62,7 +85,18 @@ public class Easy : MonoBehaviour
         {
             transform.position = new Vector3(leftwarp, other.position.y, other.position.z);
         }
-        
+
+        if (other.position.y < -5.5)
+        {
+            transform.position = new Vector3(other.position.x, upwarp, other.position.z);
+        }
+
+        if (other.position.y > 5.5)
+        {
+            transform.position = new Vector3(other.position.x, downwarp, other.position.z);
+        }
+        // är koderna som gör så att om skeppet åker till ett särskilt punkt, kommer spawnas i ett annat punkt. alltså "warpar".
+
 
     }
 }
